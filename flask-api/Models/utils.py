16b592acb93dc67439054255b57022a4614247a5
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-04-13 12:17:57 -0600

    Relationship util created

--- flask-api/Models/utils.py
+++ flask-api/Models/utils.py
@@ -29,8 +29,28 @@ class Node:
         return conn.run(query)
 
 
-def checker(requiredFields: dict, data: dict):
-    for key, value in requiredFields.items():
+class Relationship:
+    def __init__(self, start_node, end_node, relationship_type, properties=None):
+        self.start_node = start_node
+        self.end_node = end_node
+        self.relationship_type = relationship_type
+        self.properties = properties if properties else {}
+
+    def create(self):
+        query = f"MATCH (a:{self.start_node.label} {{uuid: '{self.start_node.uuid}'}}), (b:{self.end_node.label} {{uuid: '{self.end_node.uuid}'}}) CREATE (a)-[r:{self.relationship_type} {self.properties}]->(b) RETURN r"
+        return conn.run(query)
+
+    def match(self):
+        query = f"MATCH (a:{self.start_node.label} {{uuid: '{self.start_node.uuid}'}})-[r:{self.relationship_type}]->(b:{self.end_node.label} {{uuid: '{self.end_node.uuid}'}}) RETURN r"
+        return conn.run(query)
+
+    def delete(self):
+        query = f"MATCH (a:{self.start_node.label} {{uuid: '{self.start_node.uuid}'}})-[r:{self.relationship_type}]->(b:{self.end_node.label} {{uuid: '{self.end_node.uuid}'}}) DELETE r"
+        return conn.run(query)
+
+
+def propChecker(fields: dict, data: dict):
+    for key, value in fields.items():
         expected_type, description, required = value
         if key not in data.keys():
             if required:
@@ -39,3 +59,7 @@ def checker(requiredFields: dict, data: dict):
             if not isinstance(data[key], expected_type):
                 return f"Incorrect type for key {key} ({description}): Expected {expected_type.__name__}, got {type(data[key]).__name__}"
     return True
+
+
+def propFilter(fields: dict, filter: list):
+    return {k: v for k, v in fields.items() if k in filter}
---
+++ flask-api/README.md
@@ -0,0 +1,6 @@
+# fraud-detection flask-api
+Bank Fraud Detection Application
+
+TODO:
+
+- Consider adding a constraint over Bank, DPI

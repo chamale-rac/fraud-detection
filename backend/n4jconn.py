65fe197ef1c20dfdfe83bbcbf82d186a0c5dd5ae
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-04-12 19:58:57 -0600

    Custom secure env access function

--- backend/n4jconn.py
+++ backend/n4jconn.py
@@ -1,16 +1,28 @@
-import os
+"""
+@file n4jconn.py
+@author Samuel Chamalé
+@date 4/12/2024
+@brief This file contains the class n4jconn, which is used to connect to the Neo4j database.
+"""
+
 import logging
 from dotenv import load_dotenv
+from utils import env
 from neo4j import GraphDatabase
 
 
-class Neo4jConnection:
+class n4jconn:
+    """
+    @class Neo4jConnection
+    @brief This class is used to connect to the Neo4j database.
+    """
+
     def __init__(self):
         load_dotenv(".env")
 
-        self._uri = os.getenv("NEO4J_URI")
-        self._username = os.getenv("NEO4J_USER")
-        self._password = os.getenv("NEO4J_PASSWORD")
+        self._uri = env("FRAUD_DATABASE_URI")
+        self._username = env("FRAUD_DATABASE_USER")
+        self._password = env("FRAUD_DATABASE_PASSWORD")
 
         if not all([self._uri, self._username, self._password]):
             raise ValueError("Missing necessary environment variables.")
@@ -25,11 +37,22 @@ class Neo4jConnection:
             raise
 
     def close(self):
+        """
+        @fn close
+        @brief This function closes the driver.
+        """
         if self._driver:
             self._driver.close()
             logging.info("Driver closed.")
 
     def query(self, query, parameters=None, db="neo4j"):
+        """
+        @fn query
+        @brief This function runs a query on the database.
+        @param query The query to run.
+        @param parameters The parameters to pass to the query.
+        @param db The database to run the query on.
+        """
         if self._driver is None:
             raise RuntimeError("Driver not initialized, query cannot be run.")
 

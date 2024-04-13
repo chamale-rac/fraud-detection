608e160c94684dc104c3a76dd8fb069a0aec20b7
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-04-13 12:19:12 -0600

    add Models routes to blueprint

--- flask-api/app.py
+++ flask-api/app.py
@@ -8,10 +8,16 @@
 from flask import Flask
 from flask_cors import CORS
 
+from Models.Bank import api as bank_api
+from Models.Client import api as client_api
+
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 cors = CORS(app)
+app.register_blueprint(bank_api, url_prefix='/bank')
+app.register_blueprint(client_api, url_prefix='/client')
 
 
 @app.route('/zzz', methods=['GET'])

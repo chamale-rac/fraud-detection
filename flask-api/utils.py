b8514bb09cfda31baef73f9b86d735513061eef1
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-04-13 20:50:24 -0600

    API: Isolate utils.py

--- flask-api/utils.py
+++ flask-api/utils.py
@@ -1,62 +1,4 @@
-from n4jconn import n4jconn
-import uuid
-
-conn = n4jconn()
-
-
-class Node:
-    def __init__(self, label, properties: dict):
-        self.label = label
-        self.properties = properties
-        self.uuid = str(uuid.uuid4())
-
-    def create(self):
-        properties = self.properties
-        properties['uuid'] = self.uuid
-        query = f"CREATE (n:{self.label} {dict2Cypher(properties)})"
-        return conn.run(query)
-
-    def merge(self):
-        query = f"""
-        MERGE (n:{self.label} {dict2Cypher(self.properties)})
-        ON CREATE SET n.uuid = '{self.uuid}'
-        RETURN n.uuid as uuid
-        """
-        response = conn.run(query)
-        self.uuid = response["response"][0]['uuid']
-        return response
-
-    def match(self):
-        query = f"MATCH (n:{self.label} {dict2Cypher(self.properties)}) RETURN n"
-        return conn.run(query)
-
-    def delete(self):
-        query = f"MATCH (n:{self.label} {self.properties}) DELETE n"
-        return conn.run(query)
-
-    def getAll(self):
-        query = f"MATCH (n:{self.label}) RETURN n"
-        return conn.run(query)
-
-
-class Relationship:
-    def __init__(self, start_node, end_node, relationship_type, properties=None):
-        self.start_node = start_node
-        self.end_node = end_node
-        self.relationship_type = relationship_type
-        self.properties = properties if properties else {}
-
-    def create(self):
-        query = f"MATCH (a:{self.start_node.label} {{uuid: '{self.start_node.uuid}'}}), (b:{self.end_node.label} {{uuid: '{self.end_node.uuid}'}}) CREATE (a)-[r:{self.relationship_type} {self.properties}]->(b) RETURN r"
-        return conn.run(query)
-
-    def match(self):
-        query = f"MATCH (a:{self.start_node.label} {{uuid: '{self.start_node.uuid}'}})-[r:{self.relationship_type}]->(b:{self.end_node.label} {{uuid: '{self.end_node.uuid}'}}) RETURN r"
-        return conn.run(query)
-
-    def delete(self):
-        query = f"MATCH (a:{self.start_node.label} {{uuid: '{self.start_node.uuid}'}})-[r:{self.relationship_type}]->(b:{self.end_node.label} {{uuid: '{self.end_node.uuid}'}}) DELETE r"
-        return conn.run(query)
+from datetime import datetime
 
 
 def propChecker(fields: dict, data: dict):
@@ -76,10 +18,35 @@ def propFilter(fields: dict, filter: list):
 
 
 def dict2Cypher(properties: dict):
-    return '{' + ', '.join(f"{k}: '{v}'" for k, v in properties.items()) + '}'
+    cypher_properties = []
+    for k, v in properties.items():
+        if isinstance(v, str):
+            cypher_properties.append(f"{k}: '{v}'")
+        elif isinstance(v, (int, float, bool)):
+            cypher_properties.append(f"{k}: {v}")
+        elif isinstance(v, list):
+            cypher_properties.append(f"{k}: {v}")
+        elif isinstance(v, dict):
+            cypher_properties.append(f"{k}: {dict2Cypher(v)}")
+        elif isinstance(v, datetime):
+            casted = v.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+            cypher_properties.append(f"{k}: datetime('{casted}')")
+        else:
+            raise ValueError(f"Unsupported value type: {type(v)}")
+    return '{' + ', '.join(cypher_properties) + '}'
 
 
 def node2Dict(node):
     node_dict = {prop: node[prop] for prop in node.keys()}
     node_dict['labels'] = list(node.labels)
     return node_dict
+
+
+def convertDate(data_string):
+    '''
+    data_string example: "2024-04-12", "%Y-%m-%d"
+    '''
+    try:
+        return datetime.datetime.strptime(data_string, "%Y-%m-%d")
+    except ValueError:
+        return None

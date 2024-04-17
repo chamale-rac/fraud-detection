0c590b60ffae9aec8a6700cd7d02257154ff2a5c
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-04-17 13:31:36 -0600

    API: GDS algorithm route

--- flask-api/Routes/Admin.py
+++ flask-api/Routes/Admin.py
@@ -8,7 +8,7 @@
 from flask import Blueprint, request, jsonify
 from flask_cors import CORS
 from Generics import cur
-
+from utils import propChecker
 
 api = Blueprint("admin", __name__)
 cors = CORS(api)
@@ -85,4 +85,103 @@ def getTypes():
 
     return jsonify({
         "message": f"Failed to get transaction types with error: {response['message']}"
+    }), 400
+
+
+deleteProperties = {
+    "group_size": (int, "Group size", True)
+}
+
+
+@api.route("/delete_fraud_clients", methods=["POST"])
+def deleteFraudClients():
+    data = request.get_json()
+
+    valid, message = propChecker(deleteProperties, data)
+
+    if not valid:
+        return jsonify({
+            "message": message
+        }), 400
+
+    group_size = data["group_size"]
+
+    group_size = data["group_size"]
+
+    query = f"""
+    CALL gds.graph.drop('wcc')
+    YIELD graphName;
+
+    CALL gds.graph.project('wcc',
+      {{
+        Client: {{
+          label: 'Client'
+        }}
+      }},
+      {{
+        SHARED_IDENTIFIERS:{{
+          type: 'SHARED_IDENTIFIERS',
+          orientation: 'UNDIRECTED',
+          properties: {{
+            count: {{
+              property: 'count'
+            }}
+          }}
+        }}
+      }}
+    ) YIELD graphName,nodeCount,relationshipCount,projectMillis;
+
+    CALL gds.wcc.stream('wcc',
+      {{
+        nodeLabels: ['Client'],
+        relationshipTypes: ['SHARED_IDENTIFIERS'],
+        consecutiveIds: true
+      }}
+    )
+    YIELD componentId, nodeId
+    WITH componentId AS cluster, gds.util.asNode(nodeId) AS client
+    WITH cluster, collect(client.uuid) AS clients
+    WITH cluster, clients, size(clients) AS clusterSize WHERE clusterSize > 1
+    UNWIND clients AS client
+    MATCH (c:Client) WHERE c.uuid = client
+    SET c.firstPartyFraudGroup=cluster;
+
+
+    // Match the clients based on group criteria
+
+    MATCH (c:Client)
+    WITH c.firstPartyFraudGroup AS fpGroupID, collect(c.uuid) AS fGroup
+    WITH *, size(fGroup) AS groupSize WHERE groupSize >= {group_size}
+    WITH collect(fpGroupID) AS fraudRings
+
+    // Count the nodes to be deleted
+    MATCH (c:Client)
+    WHERE c.firstPartyFraudGroup IN fraudRings
+    WITH fraudRings, count(c) AS nodesToDelete
+
+    // Perform the deletion
+    MATCH (c:Client)
+    WHERE c.firstPartyFraudGroup IN fraudRings
+    OPTIONAL MATCH (c)-[r]-()
+    DELETE r, c
+
+    // Return the count of deleted nodes
+    RETURN nodesToDelete;
+    """
+
+    response = conn.run(query)
+
+    if response["success"]:
+        if len(response["response"]) == 0:
+            return jsonify({
+                "message": "No fraud clients deleted"
+            }), 200
+
+        return jsonify({
+            "message": "Fraud clients deleted",
+            "count": response["response"][0]["nodesToDelete"]
+        }), 200
+
+    return jsonify({
+        "message": f"Failed to delete fraud clients with error: {response['message']}"
     }), 400

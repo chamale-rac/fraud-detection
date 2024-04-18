574c4e2955b2d0c431347a7b09d7955cc09f0e52
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-04-17 19:25:36 -0600

    upload csv

--- generator/uploader.py
+++ generator/uploader.py
@@ -1,7 +1,7 @@
 from n4jconn import n4jconn
 
 uploadBank = """
-LOAD CSV WITH HEADERS FROM 'https://github.com/chamale-rac/fraud-detection/tree/main/generator/generated_data/fake_bank.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/chamale-rac/fraud-detection/main/generator/generated_data/fake_bank.csv' AS row
 CREATE (b:Bank {
     uuid: row.uuid,
     name: row.name,
@@ -14,14 +14,14 @@ CREATE (b:Bank {
 """
 
 uploadEmployee = """
-LOAD CSV WITH HEADERS FROM 'https://github.com/chamale-rac/fraud-detection/tree/main/generator/generated_data/fake_employees.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/chamale-rac/fraud-detection/main/generator/generated_data/fake_employees.csv' AS row
 MATCH (b:Bank {uuid: row.bank_uuid})
 CREATE (e:Employee {
     uuid: row.uuid,
     name: row.name,
     surname: row.surname,
     password: row.password,
-    birthday: date(row.birthday),
+    birthday: row.birthday,
     genre: row.genre,
     phone: row.phone,
     email: row.email,
@@ -31,18 +31,18 @@ CREATE (e:Employee {
     state: row.state,
     country: row.country,
     postal_code: row.postal_code
-})<-[:HAS_EMPLOYEE {position: row.position, salary: toFloat(row.salary), since: date(row.since)}]-(b)
+})<-[:HAS_EMPLOYEE {position: row.position, salary: toFloat(row.salary), since: row.since}]-(b)
 """
 
 uploadClient = """
-LOAD CSV WITH HEADERS FROM 'https://github.com/chamale-rac/fraud-detection/tree/main/generator/generated_data/fake_clients.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/chamale-rac/fraud-detection/main/generator/generated_data/fake_clients.csv' AS row
 MATCH (b:Bank {uuid: row.bank_uuid}), (e:Employee {uuid: row.employee_uuid})
 CREATE (c:Client {
     uuid: row.uuid,
     name: row.name,
     surname: row.surname,
     password: row.password,
-    birthday: date(row.birthday),
+    birthday: row.birthday,
     genre: row.genre,
     phone: row.phone,
     email: row.email,
@@ -60,7 +60,7 @@ CREATE (c:Client {
 """
 
 uploadAccount = """
-LOAD CSV WITH HEADERS FROM 'https://github.com/chamale-rac/fraud-detection/tree/main/generator/generated_data/fake_accounts.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/chamale-rac/fraud-detection/main/generator/generated_data/fake_accounts.csv' AS row
 MATCH (b:Bank {uuid: row.bank_uuid}), (c:Client {uuid: row.user_uuid})
 CREATE (a:Account {
   account_type: row.account_type,
@@ -68,14 +68,14 @@ CREATE (a:Account {
   nickname: row.nickname,
   interest_rate: toFloat(row.interest_rate),
   status: row.status,
-  open_date: date(row.open_date),
+  open_date: row.open_date,
   balance: toFloat(row.balance)
 })<-[:OWNS_ACCOUNT]-(c),
    (b)-[:HAS_ACCOUNT]->(a)
 """
 
 uploadSomeRelationsClients = """
-LOAD CSV WITH HEADERS FROM 'https://github.com/chamale-rac/fraud-detection/tree/main/generator/generated_data/fake_clients.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/chamale-rac/fraud-detection/main/generator/generated_data/fake_clients.csv' AS row
 MATCH (c:Client {uuid: row.uuid})
 MERGE (d:DPI {value: row.dpi})
 MERGE (e:Email {value: row.email})
@@ -88,28 +88,35 @@ CREATE (c)-[:HAS_ADDRESS]->(a)
 """
 
 uploadSomeRelationsEmployees = """
-LOAD CSV WITH HEADERS FROM 'https://github.com/chamale-rac/fraud-detection/tree/main/generator/generated_data/fake_employees.csv' AS row
-MATCH (e:Employee {uuid: row.uuid})
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/chamale-rac/fraud-detection/main/generator/generated_data/fake_employees.csv' AS row
+MATCH (emp:Employee {uuid: row.uuid})
 MERGE (d:DPI {value: row.dpi})
 MERGE (e:Email {value: row.email})
 MERGE (p:Phone {value: row.phone})
 MERGE (a:Address {street: row.street, city: row.city, state: row.state, country: row.country, postal_code: row.postal_code})
-CREATE (e)-[:HAS_DPI]->(d)
-CREATE (e)-[:HAS_EMAIL]->(e)
-CREATE (e)-[:HAS_PHONE]->(p)
-CREATE (e)-[:HAS_ADDRESS]->(a)
+CREATE (emp)-[:HAS_DPI]->(d)
+CREATE (emp)-[:HAS_EMAIL]->(e)
+CREATE (emp)-[:HAS_PHONE]->(p)
+CREATE (emp)-[:HAS_ADDRESS]->(a)
 """
 
 conn = n4jconn()
 
 
 def uploadData():
-    conn.query(uploadBank)
-    conn.query(uploadEmployee)
-    conn.query(uploadClient)
-    conn.query(uploadAccount)
-    conn.query(uploadSomeRelationsClients)
-    conn.query(uploadSomeRelationsEmployees)
+    print("Uploading data...")
+    print("This may take a while...")
+    print("Uploading banks...")
+    conn.run(uploadBank)
+    print("Uploading employees...")
+    conn.run(uploadEmployee)
+    print("Uploading clients...")
+    conn.run(uploadClient)
+    print("Uploading accounts...")
+    conn.run(uploadAccount)
+    print("Uploading some relations...")
+    conn.run(uploadSomeRelationsClients)
+    conn.run(uploadSomeRelationsEmployees)
 
 
 uploadData()
